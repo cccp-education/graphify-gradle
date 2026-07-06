a1193de192de019d67c005ae8470f87b9a1e45c2
cheroliv <cheroliv@users.noreply.github.com>
2026-07-06 19:40:38 +0200

    chore(cnv): bump education.cccp.build.* 0.0.1 -> 0.0.2 (harmonisation conventions-gradle)

--- graphify-plugin/build.gradle.kts
+++ graphify-plugin/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     `java-library`
     alias(libs.plugins.publish)
-    id("education.cccp.build.gradle-plugin") version "0.0.1"
-    id("education.cccp.build.publishing") version "0.0.1"
+    id("education.cccp.build.gradle-plugin") version "0.0.2"
+    id("education.cccp.build.publishing") version "0.0.2"
 }
 
 group = "education.cccp"
